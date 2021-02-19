790874d0a95b6a41d716ef8e02b5ce674f309d70
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-02-19 15:10:45 +0100

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,38 @@
 import type { NextPage } from "next"
-import React from "react"
 
-const IndexPage: NextPage = () => <h1>Hola mundo</h1>
+const IndexPage: NextPage = () => (
+  <div className="wrapper">
+    <style jsx>{`
+      :global(body) {
+        background-color: rgb(250, 250, 250);
+      }
+
+      .wrapper {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+          Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+          sans-serif;
+
+        text-align: left;
+        color: rgb(90, 90, 90);
+        font-weight: bold;
+        margin: 20vh 20vw 0;
+      }
+
+      a {
+        color: inherit;
+      }
+    `}</style>
+    <h1>
+      Estamos
+      <br />
+      construyendo
+      <br />
+      materia.eco
+    </h1>
+    <p>
+      <a href="mailto:hola@materia.eco">hola@materia.eco</a>
+    </p>
+  </div>
+)
 
 export default IndexPage
