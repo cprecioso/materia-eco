deb58b3174646e576050e03a62adb0806cd19760
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-02-24 21:25:21 +0100

    More lightweight icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,12 +1,26 @@
-import { config } from "@fortawesome/fontawesome-svg-core"
-import {
-  FontAwesomeIcon,
-  FontAwesomeIconProps,
-} from "@fortawesome/react-fontawesome"
+import type { IconDefinition } from "@fortawesome/fontawesome-common-types"
 import type { FunctionComponent } from "react"
 
-config.autoAddCss = false
+export const Icon: FunctionComponent<{
+  icon: IconDefinition
+  width?: string
+  height?: string
+}> = ({ icon, height = "1em", width = "1em" }) => {
+  const [viewBoxWidth, viewBoxHeight, , , paths] = icon.icon
 
-export const Icon: FunctionComponent<FontAwesomeIconProps> = (props) => (
-  <FontAwesomeIcon width="1em" height="1em" {...props} />
-)
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}
+      width={width}
+      height={height}
+    >
+      {(typeof paths === "string" ? [paths] : paths).map((path, i) => (
+        <path key={i} fill="currentColor" d={path} />
+      ))}
+    </svg>
+  )
+}
