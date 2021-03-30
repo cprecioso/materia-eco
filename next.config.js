ca833dc8e52c0195f0c6eca96aca527702285e72
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-03-30 12:37:50 +0200

    Add type checking to Next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+// @ts-check
+
+module.exports = /** @type {import("next/dist/next-server/server/config-shared").NextConfig} */ ({
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
   },
-}
+})
