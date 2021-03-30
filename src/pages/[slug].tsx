907318670336dbaaebcede70b2805f33f6090757
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-03-30 17:54:04 +0200

    Get SEO data from DatoCMS

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,3 +1,4 @@
+import { AppWrapper } from "@/components/AppWrapper"
 import { EmailInput } from "@/components/EmailInput"
 import { MenuData, MenuDocument, MenuProvider } from "@/components/Menu"
 import { NavBar } from "@/components/NavBar"
@@ -10,8 +11,7 @@ import {
   SiteLocale,
 } from "@/__generated/graphql"
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next"
-import Head from "next/head"
-import { renderMetaTags, SeoMetaTagType, StructuredText } from "react-datocms"
+import { StructuredText } from "react-datocms"
 
 type Query = { slug: string }
 
@@ -47,7 +47,11 @@ export const getStaticPaths: GetStaticPaths<Query> = async ({ locales }) => {
 
 gql`
   query GetMarketingPage($slug: String, $locale: SiteLocale) {
+    ...AppWrapperData
     marketingPage(filter: { slug: { eq: $slug } }, locale: $locale) {
+      _seoMetaTags {
+        ...AppWrapperSeo
+      }
       name
       coverImage {
         ...HalfImage
@@ -85,33 +89,28 @@ export const getStaticProps: GetStaticProps<Props, Query> = async ({
 type Props = { response: GetMarketingPageQuery; menuData: MenuData }
 
 const MarketingPage: NextPage<Props> = ({ response, menuData }) => (
-  <MenuProvider value={menuData}>
-    <style jsx>{`
-      footer {
-        margin-bottom: 1rem;
-      }
-    `}</style>
+  <AppWrapper seo={response.marketingPage?._seoMetaTags} site={response}>
+    <MenuProvider value={menuData}>
+      <style jsx>{`
+        footer {
+          margin-bottom: 1rem;
+        }
+      `}</style>
 
-    <Head>
-      {response.marketingPage?._seoMetaTags
-        ? renderMetaTags(
-            response.marketingPage._seoMetaTags as SeoMetaTagType[]
-          )
-        : null}
-    </Head>
-    <NavBar />
-    <TwoHalvesPage
-      name={response.marketingPage?.name!}
-      image={response.marketingPage?.coverImage!}
-      footer={
-        <footer>
-          <EmailInput data={response.mailingListForm!} />
-        </footer>
-      }
-    >
-      <StructuredText data={response.marketingPage?.content?.value} />
-    </TwoHalvesPage>
-  </MenuProvider>
+      <NavBar />
+      <TwoHalvesPage
+        name={response.marketingPage?.name!}
+        image={response.marketingPage?.coverImage!}
+        footer={
+          <footer>
+            <EmailInput data={response.mailingListForm!} />
+          </footer>
+        }
+      >
+        <StructuredText data={response.marketingPage?.content?.value} />
+      </TwoHalvesPage>
+    </MenuProvider>
+  </AppWrapper>
 )
 
 export default MarketingPage
