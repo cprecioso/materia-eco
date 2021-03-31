4f90dcbad42b998b847b24f255ff2951c1ce223c
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-04-01 00:06:24 +0200

    Format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,56 +44,54 @@ export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => {
 
 type Props = { data: GetLandingPageQuery }
 
-const IndexPage: NextPage<Props> = ({ data }) => {
-  return (
-    <AppWrapper seo={data.landingPage?._seoMetaTags} site={data}>
-      <MenuProvider value={data}>
-        <div className={styles.outerWrapper}>
-          <div className={styles.contentWrapper}>
-            <header className={styles.landing}>
-              <StructuredText data={data.landingPage?.content?.value} />
-            </header>
+const IndexPage: NextPage<Props> = ({ data }) => (
+  <AppWrapper seo={data.landingPage?._seoMetaTags} site={data}>
+    <MenuProvider value={data}>
+      <div className={styles.outerWrapper}>
+        <div className={styles.contentWrapper}>
+          <header className={styles.landing}>
+            <StructuredText data={data.landingPage?.content?.value} />
+          </header>
 
-            <div className={styles.content}>
-              <Menu />
-            </div>
-
-            <EmailInput className={styles.emailInput} data={data} />
+          <div className={styles.content}>
+            <Menu />
           </div>
 
-          <div className={styles.emblems}>
-            <a href="https://profiles.eco/materia?ref=tm" rel="noopener">
-              <img
-                alt=".eco profile for materia.eco"
-                src="https://trust.profiles.eco/materia/eco-mark.svg?color=%234b4b4b"
-              />
-            </a>
-          </div>
-
-          <video
-            className={styles.background}
-            poster={data.landingPage?.background?.blurUpThumb!}
-            autoPlay
-            loop
-            disablePictureInPicture
-            disableRemotePlayback
-            muted
-            playsInline
-            controls={false}
-          >
-            <source
-              src={data.landingPage?.background?.video?.streamingUrl}
-              type="application/x-mpegURL"
-            />
-            <source
-              src={data.landingPage?.background?.video?.mp4Url!}
-              type="video/mp4"
-            />
-          </video>
+          <EmailInput className={styles.emailInput} data={data} />
         </div>
-      </MenuProvider>
-    </AppWrapper>
-  )
-}
+
+        <div className={styles.emblems}>
+          <a href="https://profiles.eco/materia?ref=tm" rel="noopener">
+            <img
+              alt=".eco profile for materia.eco"
+              src="https://trust.profiles.eco/materia/eco-mark.svg?color=%234b4b4b"
+            />
+          </a>
+        </div>
+
+        <video
+          className={styles.background}
+          poster={data.landingPage?.background?.blurUpThumb!}
+          autoPlay
+          loop
+          disablePictureInPicture
+          disableRemotePlayback
+          muted
+          playsInline
+          controls={false}
+        >
+          <source
+            src={data.landingPage?.background?.video?.streamingUrl}
+            type="application/x-mpegURL"
+          />
+          <source
+            src={data.landingPage?.background?.video?.mp4Url!}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+    </MenuProvider>
+  </AppWrapper>
+)
 
 export default IndexPage
